3d55c458622c6ecddfa76fbf23ad2988c16a20e6
maria <mariagedye@gmail.com>
2023-10-27 12:52:34 +1300

    timer added to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:timesheet_app/components/timer_button.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
+import 'dart:async';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -18,35 +19,18 @@ class _HomePageState extends State<HomePage> {
   late bool servicePermission = false;
   late LocationPermission permission;
   String _currentAddress = '';
-  bool showStartButton = true;
-  ButtonStyle style = ElevatedButton.styleFrom(
-        textStyle: TextStyle(
-          fontSize: 50,
-        ),
-        backgroundColor: Color.fromRGBO(250, 195, 32, 1),
-        foregroundColor: Color.fromRGBO(64, 46, 50, 1),
-        fixedSize: Size.fromRadius(100),
-        shape: CircleBorder(),
-        shadowColor: Colors.black,
-        elevation: 10.0,
-        side: BorderSide(
-          color: Color.fromRGBO(164, 142, 101, 1),
-          width: 10.0,
-        )
-        );
+
+  int seconds = 0, minutes = 0, hours = 0;
+  String digitSeconds = "00", digitMinutes = "00", digitHours = "00";
+  Timer? timer;
+  bool started = false;
+  List shifts = [];
 
 // state methods...
 
   //sign user out method
   void signUserOut() {
     FirebaseAuth.instance.signOut();
-  }
-
-  // toggle between start and stop buttons
-  void toggleStartStop() {
-    setState(() {
-      showStartButton = !showStartButton;
-    });
   }
 
   // get current location method
@@ -81,13 +65,72 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  // methods for timer
+  void stop() {
+    timer!.cancel();
+    setState(() {
+      started = false;
+      addShifts(); // adds entries to the shifts list
+    });
+  }
+
+  void reset() {
+    timer!.cancel();
+    setState(() {
+      seconds = 0;
+      minutes = 0;
+      hours = 0;
+
+      digitSeconds = "00";
+      digitMinutes = "00";
+      digitHours = "00";
+      started = false;
+    });
+  }
+
+  void addShifts() {
+    String shift = "$digitHours:$digitMinutes:$digitSeconds";
+    setState(() {
+      (shift != "00:00:00") ? shifts.add(shift) : null;
+      reset();
+    });
+  }
+
+  // start the timer function
+  void start() {
+    started = true;
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      int localSeconds = seconds + 1;
+      int localMinutes = minutes;
+      int localHours = hours;
+
+      if (localSeconds > 59) {
+        if (localMinutes > 59) {
+          localHours++;
+          localMinutes = 0;
+        } else {
+          localMinutes++;
+          localSeconds = 0;
+        }
+      }
+      setState(() {
+        seconds = localSeconds;
+        minutes = localMinutes;
+        hours = localHours;
+        digitSeconds = (seconds >= 10) ? "$seconds" : "0$seconds";
+        digitHours = (hours >= 10) ? "$hours" : "0$hours";
+        digitMinutes = (minutes >= 10) ? "$minutes" : "0$minutes";
+      });
+    });
+  }
+
   // builds ui as...
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color.fromRGBO(64, 46, 50, 1),
 
-        // log out
+        // log out, settings
         appBar: AppBar(
           backgroundColor: Color.fromRGBO(64, 46, 50, 1),
           actions: [
@@ -102,20 +145,13 @@ class _HomePageState extends State<HomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               // Clock in title
-              Text('Clock in',
+              Text((!started) ? 'Clock in' : 'Clock out',
                   style: TextStyle(
                     color: Color.fromRGBO(250, 195, 32, 1),
                     fontSize: 40,
                   )),
 
               SizedBox(height: 20),
-
-              // Timer display
-              Text('0:00:00',
-                  style: TextStyle(
-                    color: Color.fromRGBO(250, 195, 32, 1),
-                    fontSize: 60,
-                  )),
 
               // Current location display
               Row(
@@ -128,7 +164,9 @@ class _HomePageState extends State<HomePage> {
                   Flexible(
                     child: Column(children: [
                       Text(
-                        _currentAddress, 
+                        _currentAddress.isNotEmpty
+                            ? _currentAddress
+                            : 'unknown',
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 16,
@@ -143,24 +181,104 @@ class _HomePageState extends State<HomePage> {
 
               SizedBox(height: 20),
 
+              // Timer display
+              Text('$digitHours:$digitMinutes:$digitSeconds',
+                  style: TextStyle(
+                    color: Color.fromRGBO(250, 195, 32, 1),
+                    fontSize: 60,
+                  )),
+
               // clock in button
               ElevatedButton(
                   onPressed: () async {
                     _currentLocation = await _getCurrentLocation();
                     await _getAddressFromCoordinates();
+
                     print('$_currentLocation');
                     print(_currentAddress);
-                    // change to new state                    
+
+                    (!started) ? start() : stop();
                   },
-                  style: style,  
-                  child: Text('button'))
+                  style: ElevatedButton.styleFrom(
+                      textStyle: TextStyle(
+                        fontSize: 50,
+                      ),
+                      backgroundColor: (!started)
+                          ? Color.fromRGBO(250, 195, 32, 1)
+                          : Color.fromRGBO(64, 46, 50, 1),
+                      foregroundColor: (!started)
+                          ? Color.fromRGBO(64, 46, 50, 1)
+                          : Color.fromRGBO(250, 195, 32, 1),
+                      fixedSize: Size.fromRadius(100),
+                      shape: CircleBorder(),
+                      shadowColor: Colors.black,
+                      elevation: 10.0,
+                      side: BorderSide(
+                        color: Color.fromRGBO(164, 142, 101, 1),
+                        width: 10.0,
+                      )),
+                  child: Text((!started) ? 'Start' : 'Stop')),
+
+              SizedBox(height: 10),
+              // cancel the timer
+              TextButton(
+                  onPressed: () {
+                    (started) ? reset() : null;
+                  },
+                  style: TextButton.styleFrom(
+                    disabledForegroundColor: Colors.grey,
+                    foregroundColor: (!started)
+                        ? Color.fromRGBO(64, 46, 50, 1)
+                        : Color.fromRGBO(250, 195, 32, 1),
+                  ),
+                  child: Text(
+                    'Cancel',
+                    style: TextStyle(fontSize: 16.0),
+                  )),
 
               // total hours worked card
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  height: 160.0,
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 73, 53, 57),
+                      borderRadius: BorderRadius.circular(10.0)),
+                  //list of shifts
+                  child: ListView.builder(
+                    itemCount: shifts.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Shift# ${index + 1}",
+                              style: const TextStyle(
+                                color: Colors.amber,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                            Text(
+                              "${shifts[index]}",
+                              style: const TextStyle(
+                                color: Colors.amber,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
 
-              // Bottom Nav Bar with three buttons
-              // User button
-              // add shift button
-              // time log? timesheet?
+              // Bottom Nav Bar with three buttons:
+              // 1. User details
+              // 2. add shift (manual entry)
+              // 3. send timesheet
             ],
           )),
         )));
